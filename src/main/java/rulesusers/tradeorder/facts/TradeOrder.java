c28fd0cfbe6fb3ec098902c3131c7b2f100a5beb
Stephen Masters <stephen.masters@me.com>
2013-03-21 10:30:36 +0000

    Void change

---
+++ src/main/java/rulesusers/tradeorder/facts/TradeOrder.java
@@ -0,0 +1,34 @@
+package rulesusers.tradeorder.facts;
+
+import java.util.Date;
+
+public class TradeOrder {
+    
+    private Date orderOpenTime;
+    private String status;
+    
+    public TradeOrder() {
+    }
+    
+    public TradeOrder(Date openTime, String status) {
+        this.orderOpenTime = openTime;
+        this.status = status;
+    }
+
+    public Date getOrderOpenTime() {
+        return orderOpenTime;
+    }
+
+    public void setOrderOpenTime(Date orderOpenTime) {
+        this.orderOpenTime = orderOpenTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
