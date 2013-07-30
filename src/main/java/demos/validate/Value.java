b73725948ea690f8a53ade65b34bac15c05e7455
Stephen Masters <stephen.masters@me.com>
2013-07-30 14:32:42 +0100

    Putting property change listener stuff back in

--- src/main/java/demos/validate/Value.java
+++ src/main/java/demos/validate/Value.java
@@ -1,9 +1,22 @@
 package demos.validate;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class Value<T> {
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    
     private T value;
     private boolean valid;
     
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        this.pcs.removePropertyChangeListener(listener);
+    }
+
     public T getValue() {
         return value;
     }
@@ -11,6 +24,8 @@ public class Value<T> {
     public void setValue(T value) {
         T oldValue = this.value;
         this.value = value;
+
+        pcs.firePropertyChange("value", oldValue, value);
     }
 
     public boolean isValid() {
@@ -20,5 +35,7 @@ public class Value<T> {
     public void setValid(boolean valid) {
         boolean oldValue = this.valid;
         this.valid = valid;
+
+        pcs.firePropertyChange("valid", oldValue, valid);
     }
 }
